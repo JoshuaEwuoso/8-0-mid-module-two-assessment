1380cb9ae41f566a9cf588871e09ee750b8d9136
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-09-09 11:47:56 -0400

    completes tomatoScore

--- index.js
+++ index.js
@@ -186,7 +186,16 @@ function getAllMoviesReleasedAtOrBeforeYear(movies, year) {
       { "James and the Giant Peach": "91%" },
     ];
  */
-function getRottenTomatoesScoreByMovie() {}
+function getRottenTomatoesScoreByMovie(movies) {
+  if(!movies.length) {
+    throw "Movies array is empty"
+  }
+  return movies.map(movie => {
+    let result = {};
+    result[movie.title] = movie.ratings.find(rating => rating.source === "Rotten Tomatoes").value
+    return result
+  })
+  }
 
 // Do not change anything below this line.
 module.exports = {
